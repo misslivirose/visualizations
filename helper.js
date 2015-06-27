5520c5a432a02349dac17be26721fcd15d0cb38c
Liv Erickson <livi@outlook.com>
2015-06-27 13:44:13 -0700

    added color picker

--- helper.js
+++ helper.js
@@ -10,3 +10,38 @@ var spiral = function(segLength, size)
 	}
 	return geometry;
 };
+
+/**
+ * Random shape function to test out different values
+ */
+var randomGraph = function (segLength, size)
+{
+	var geometry = new THREE.Geometry();
+	for(var t = 0; t < size; t+= segLength)
+	{
+		var _x = Math.cos(t) / Math.sin(t) + t*(Math.sin(1/t));
+		var _y = Math.sin(t) / Math.cos(t) + t*(Math.cos(1/t));
+		geometry.vertices.push(new THREE.Vector3(_x, _y, 1));
+	}	
+	return geometry;
+};
+
+var definedShape = function(pointArr)
+{
+	var geometry = new THREE.Geometry();
+	for (var i = 0; i < pointArr.length; i++)
+	{
+		geometry.vertices.push(pointArr[i]);
+	}
+	return geometry;
+};
+
+var point = function(x, y, z)
+{
+	if(z === undefined)
+	{
+		z === 10;
+	}
+	var _p = [x, y, z];
+	return _p;
+};
